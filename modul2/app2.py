984e964b6a11b1d98d9cb03af0492365fbf843f1
emcraciu <ozy24us@gmail.com>
2024-01-29 20:00:55 +0200

    modul 2

---
+++ modul2/app2.py
@@ -0,0 +1,12 @@
+h = input('Input height:')
+b = input('Input base:')
+h = int(h)
+b = int(b)
+
+A = (1/2) * b * h
+print(A)
+
+
+# prn = print('out')
+# print(prn)
+# print(type(prn))
---
+++ modul2/part1_2.py
@@ -0,0 +1,29 @@
+int1 = 100
+int2 = 200
+
+result = int1 + int2
+print(result)
+
+new_result = result - int1
+print(new_result)
+
+sqrt = int1 ** (1/2)
+print(sqrt)
+print(type(sqrt))
+
+# float
+add1 = 10.1 + 10.2
+print(add1)
+
+add_result = (10).__add__(10)
+print(add_result)
+
+# modulo
+modulo_result = 10 % 3
+print(modulo_result)
+
+result = 10 // 3
+print(result)
+
+result = 10 / 3
+print(result)
