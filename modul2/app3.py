773e0eb9eaebe4c239ed0740b072cb8b6836b1a5
emcraciu <ozy24us@gmail.com>
2024-01-29 21:00:09 +0200

    modul 2

--- modul2/app3.py
+++ modul2/app3.py
@@ -2,5 +2,8 @@ password = 7710
 user_pass = input('Enter Password: ')
 print(user_pass)
 
+# example2
 print(str(password) == user_pass)
+
+# example1
 print(password == int(user_pass))
---
+++ modul2/app4.py
@@ -0,0 +1,12 @@
+numar_1 = int(input('nr1: '))
+numar_2 = int(input('nr2: '))
+
+print((numar_1 + numar_2) / 2)
+
+# Integer division
+int_division = numar_1 // numar_2
+print("Integer division =", int_division)
+
+# num1 ** num2
+power = numar_1 ** numar_2
+print("num1 ** num2 =", power)
