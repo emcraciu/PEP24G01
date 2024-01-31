6e87acd0b916d057ba859ba23281aadfca34547e
emcraciu <ozy24us@gmail.com>
2024-01-31 18:40:09 +0200

    modul 2.2

---
+++ modul2/app5.py
@@ -0,0 +1,6 @@
+net = input('Venit:')
+int_net = int(net)
+print('Recomandarile noastre:')
+print('Cheltuieli uzuale:', int_net * 50 * (1 / 100))
+print('Recreere:', int_net * 30 * (1 / 100))
+print('Economii si datorii:', int_net * 20 * (1 / 100))
---
+++ modul2/part2_1.py
@@ -0,0 +1,16 @@
+# complex
+
+complex_number1 = 1 + 3j
+complex_number2 = 2 + 4j
+
+result = complex_number1 + complex_number2
+print(
+    result
+)
+print(type(result))
+print(complex(10))
+
+result = 10+3j + 7
+print(result)
+print(type(result))
+
