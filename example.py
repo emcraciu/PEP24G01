30fa86e6f9b118ca41a36ac8f967612c06b54c40
emcraciu <ozy24us@gmail.com>
2024-02-07 18:29:05 +0200

    example

---
+++ example.py
@@ -0,0 +1,25 @@
+# number = int(input('give number: '))
+# print(number > 100)
+# # print(True) if number > 100 else print(False)
+
+# text = input('give str: ')
+# if text == 'Spathiphyllum':
+#     print(f'Yes - {text} is the best plant ever!')
+# elif text == 'spathiphyllum':
+#     print(f'No, I want a big {text.capitalize()}!')
+# else:
+#     print('Spathiphyllum! Not [input]!')
+
+
+# y = int(input('year:'))
+#
+# if y < 1582:
+#     print('Not within the Gregorian calendar period')
+# elif not (y % 400):
+#     print('Leap year')
+# elif not (y % 100):
+#     print('Leap year')
+# elif not (y % 4):
+#     print('Leap year')
+# else:
+#     print('Common year')
