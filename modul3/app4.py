71a51ceee6e3960d48adb89ed974bbd60206f5a9
emcraciu <ozy24us@gmail.com>
2024-02-07 20:59:59 +0200

    example

---
+++ modul3/app4.py
@@ -0,0 +1,17 @@
+parola = 7788
+# for incercare in range(99999):
+#     parola_introdusa = int(input('Introduceti parola: '))
+#     if parola == parola_introdusa :
+#        print('access granted')
+#        break
+#     else:
+#         print('Parola gresita')
+
+
+while True:
+    parola_introdusa = int(input('Introduceti parola: '))
+    if parola == parola_introdusa:
+        print('access granted')
+        break
+    else:
+        print('Parola gresita')
