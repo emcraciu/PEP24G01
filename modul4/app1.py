e21c9db2625920eec240b38af2199955919895c5
emcraciu <ozy24us@gmail.com>
2024-02-14 20:42:52 +0200

    module 4.1

--- modul4/app1.py
+++ modul4/app1.py
@@ -2,17 +2,25 @@ def check_passwd():
     requires_chars = ["!", "@", "%"]
     passwd = input("Introduceti o parola : ")
     print(passwd)
+    condition_not_ok = False
     if len(passwd) < 7:
         print("Parola cu lungime gresita")
-        check_passwd()
+        condition_not_ok = True
+
+    for character in requires_chars:
+        if character in passwd:
+            break
     else:
+        print("Parola trebuie sa contina : ! @ %")
+        condition_not_ok = True
 
-        for character in requires_chars:
-            if character in passwd:
-                break
-        else:
-            print("Parola trebuie sa contina : ! @ %")
-            check_passwd()
-    print('test')
+    for character in range(10):
+        if str(character) in passwd:
+            break
+    else:
+        print("Parola trebuie sa contina : number")
+        condition_not_ok = True
 
+    if condition_not_ok:
+        check_passwd()
 check_passwd()
