7e6263503d64ff243a3e369488cb8d354773eb64
mben23 <bmarisescu@gmail.com>
2024-02-21 18:26:52 +0200

    EX tema

---
+++ homework/tema_modul3_Beni Marisescu.py
@@ -0,0 +1,8 @@
+def fx_and_y():
+    for x in range(10, 21):
+        y = 3 * x
+        equation_result = 3 * (x**2) - 4 * y + 4
+        print(f"============= x = {x} =============")
+        print(f"Rezultatul functiei : {equation_result}")
+
+fx_and_y()
