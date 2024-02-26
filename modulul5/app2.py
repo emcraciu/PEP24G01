f8723ed41e5ec109b09139c6bb55b1311dba0029
mben23 <bmarisescu@gmail.com>
2024-02-26 14:35:48 +0200

    EX tema

--- modulul5/app2.py
+++ modulul5/app2.py
@@ -1,23 +1,26 @@
 lst = []
 while True:
-    numar = input("Introduceti un numar (cand v-ati saturat,apasati q): ")
+    numar = input("Introduceti un numar (cand v-ati saturat, apasati q): ")
     if numar == 'q':
         break
     try:
         numar = int(numar)
     except ValueError:
         print('Number not valid: ')
-    numar = int(numar)
+        continue
     lst.append(numar)
 # Suma numarului de pe pozitia 1 si 2.
 print("O sa adaugam numarul 2 si 3.")
 try:
     print("lst[1] + lst[2] = ", lst[1] + lst[2])
 except IndexError:
-    print("Not enought number")
+    print('Not enough numbers')
 # Divizia primelor 2 numere din lista
 print("Divizia primelor 2 numere din lista este: ")
-print("lst[0] / lst[1] = ", lst[0] / lst[1])
+try:
+    print("lst[0] / lst[1] = ", lst[0] / lst[1])
+except ZeroDivisionError:
+    print('Result is infinite')
 # Suma tuturor numerelor din lista
 sum = 0
 for i in lst:
