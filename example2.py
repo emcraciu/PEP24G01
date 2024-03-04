99941838e9801f62280633204a9ba95de2c23362
emcraciu <ozy24us@gmail.com>
2024-03-04 18:28:34 +0200

    module 6

---
+++ example2.py
@@ -0,0 +1,7 @@
+def add(x, y):
+    return x+y
+
+print(add)
+print(add(2, 3))
+
+result = add(3, 3)
--- modul6/app1.py
+++ modul6/app1.py
@@ -4,7 +4,30 @@ class Shop:
     user_input = None
     status = None
 
-    def modif_stoc(self):
+    # def modif_stoc(self):
+    #     print('''
+    #     Meniu:
+    #            1. Vizualizare stoc
+    #            2. Adaugare produs
+    #            3. Stergere produs
+    #            4. Iesire
+    #     ''')
+    #     self.user_input = input(f'Alege optiune:\n')
+    #     if self.user_input == '2':
+    #         produs, pret, stoc = input('give product, price, stoc').split(',')
+    #         self.adauga_prod(produs, pret, stoc)
+
+    def adauga_prod(self, produs, pret, stoc):
+        self.stoc.update({produs: (pret, stoc)})
+
+    def set_status(self, status):
+        self.status = status
+
+    def show_stock(self):
+        print(self.stoc)
+
+    def start(self):
+
         print('''   
         Meniu:
                1. Vizualizare stoc
@@ -13,20 +36,28 @@ class Shop:
                4. Iesire
         ''')
         self.user_input = input(f'Alege optiune:\n')
-        if self.user_input == '2':
-            produs, pret, stoc = input('give product, price, stoc').split(',')
-            self.adauga_prod(produs, pret, stoc)
-
-    def adauga_prod(self, produs, pret, stoc):
-        self.stoc.update({produs: (pret, stoc)})
-
-    def set_status(self, status):
-        self.status = status
+        while self.user_input != '4':
+            if self.user_input == '1':
+                self.show_stock()
+            elif self.user_input == '2':
+                produs, pret, stoc = input('give product, price, stoc').split(',')
+                self.adauga_prod(produs, pret, stoc)
+            elif self.user_input == '3':
+                pass
+                ## implement
+            print('''   
+            Meniu:
+                   1. Vizualizare stoc
+                   2. Adaugare produs
+                   3. Stergere produs
+                   4. Iesire
+            ''')
+            self.user_input = input(f'Alege optiune:\n')
 
 
 
 s = Shop()
-s.modif_stoc()
+s.start()
 # t = Shop()
 # Shop.adauga_prod(s, 'unt', 5, 100)
 # s.adauga_prod('unt', 5, 100)
@@ -38,5 +69,5 @@ s.modif_stoc()
 # print(f'Ai ales: {s.user_input}')
 # print(s.stoc)
 # print(t.stoc)
-print(s.stoc)
+# print(s.show_stock())
 # print(t.status)
