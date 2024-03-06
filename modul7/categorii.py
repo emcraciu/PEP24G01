5ba39fe2296a4d7a401d079e2c528ad709540a95
emcraciu <ozy24us@gmail.com>
2024-03-06 19:20:42 +0200

    module 6

---
+++ modul7/categorii.py
@@ -0,0 +1,21 @@
+class Categorii:
+    def __init__(self, nume, pret, stoc):
+        self.nume = nume
+        self.pret = pret
+        self.stoc = stoc
+
+
+class Haine(Categorii):
+    pass
+
+
+class Accesorii(Categorii):
+    pass
+
+
+class Incaltaminte(Categorii):
+    pass
+
+# tricouri = Haine("tricou", 20, 35)
+# print(tricouri.__class__.__name__)
+# print(type(tricouri))
