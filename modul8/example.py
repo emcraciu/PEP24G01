f0c021c510d551924b1546beee015f6c6389d9fd
emcraciu <ozy24us@gmail.com>
2024-03-18 19:00:23 +0200

    module 6

--- modul8/example.py
+++ modul8/example.py
@@ -1,4 +1,8 @@
 # map all objects to letter "a"
 
 all_objects = input('provide letters: ')
-print(map())
+print(''.join(map(lambda letter: 'a', all_objects)))
+
+# map odd objects to letter "a" and even objects to letter 'b'
+all_objects = input('provide letters: ')
+print(''.join(map(lambda letter: 'a' if letter[0] % 2 else 'b', enumerate(all_objects))))
