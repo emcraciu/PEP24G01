e0749eba1f02af4d372aca9b172ee05084c03920
emcraciu <ozy24us@gmail.com>
2024-03-18 19:03:12 +0200

    module 6

--- modul8/example.py
+++ modul8/example.py
@@ -6,3 +6,11 @@ print(''.join(map(lambda letter: 'a', all_objects)))
 # map odd objects to letter "a" and even objects to letter 'b'
 all_objects = input('provide letters: ')
 print(''.join(map(lambda letter: 'a' if letter[0] % 2 else 'b', enumerate(all_objects))))
+
+# map objects to index of iterated object
+# 'abcd'
+# '0123'
+
+all_objects = input('provide letters: ')
+# print(''.join(map(lambda letter: #, enumerate(all_objects))))
+
