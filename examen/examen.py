7dd0fad3cbd6f4815e5d01823074460c3e4eae1c
emcraciu <ozy24us@gmail.com>
2024-03-30 11:07:50 +0200

    other

---
+++ examen/examen.py
@@ -0,0 +1,26 @@
+"""
+A production facility needs an iterable object to keep track product assembly times.
+A class called "KeyboardProductionTimes" that will store the information needs to be created.
+Each keyboard will have a string serial number "KBXXXXXX" and timedelta (or int) for production time
+Iterating objects created with KeyboardProductionTimes will return all keyboards that had production
+time greater than 2000s
+1) 40p: Definition
+    a) 10p: Basic class structure of KeyboardProductionTimes
+    b) 10p: Basic class structure of iterator
+    c) 10p: Method to add produced keyboards with serial and time
+    d) 10p: Method that returns average keyboard production time
+2) 40p: Execution:
+    a) 10p: Create instance of KeyboardProductionTimes
+    b) 10p: Call method to add keyboard <add_keyboard(serial, <timedelta | int>)>
+        - KB023871, 3210s
+        - KB023873, 1890s
+        - KB023875, 1982s
+    c) 10p: Call method to return average production time <average_production_time()>
+    d) 10p: Iterate the created object and write each keyboard serial with production
+            time greater then 2000s in a file on a new line
+3) 20p: Documenting:
+   a) 5p: type hints for arguments
+   b) 5p: module documentation
+   c) 5p: class documentation for all classes
+   d) 5p: method documentation for all methods
+"""
